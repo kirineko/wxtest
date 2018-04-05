34b0fb09b197b14275353531a608a630fb7bfea6
kirineko <kirineko@qq.com>
2018-04-05 22:07:33 +0800

    add reply

--- handle.py
+++ handle.py
@@ -3,6 +3,8 @@
 
 import hashlib
 import web
+import reply
+import receive
 
 class Handle(object):
     def GET(self):
@@ -28,3 +30,21 @@ class Handle(object):
                 return ""
         except Exception as Argument:
             return Argument
+
+    def POST(self):
+        try:
+            webData = web.data()
+            print("Handle Post webdata is ", webData)
+            # 后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                content = "test"
+                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                return replyMsg.send()
+            else:
+                print("暂且不处理")
+                return "success"
+        except Exception as Argment:
+            return Argment
