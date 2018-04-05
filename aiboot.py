d58b720d713c507f60e1e1f637594b8b7125ac23
kirineko <kirineko@qq.com>
2018-04-05 23:17:41 +0800

    fix bugs

--- aiboot.py
+++ aiboot.py
@@ -4,4 +4,4 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 deepThought = ChatBot("deepThought")
 deepThought.set_trainer(ChatterBotCorpusTrainer)
 # 使用中文语料库训练它
-deepThought.train("chatterbot.corpus.chinese")  # 语料库
+#deepThought.train("chatterbot.corpus.chinese")  # 语料库
